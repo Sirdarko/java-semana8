68c8feb40e2d585a530ed21e21b7840499461233
eloy12334 <pendeguin12@gmail.com>
2025-10-02 10:01:15 -0300

    se crea contactos

--- src/vista/Contactos.java
+++ src/vista/Contactos.java
@@ -8,12 +8,12 @@ package vista;
  *
  * @author Santo Tomas
  */
-public class Nosotros extends javax.swing.JFrame {
+public class Contactos extends javax.swing.JFrame {
 
     /**
-     * Creates new form Nosotros
+     * Creates new form Contactos
      */
-    public Nosotros() {
+    public Contactos() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Nosotros extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Nosotros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Contactos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Nosotros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Contactos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Nosotros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Contactos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Nosotros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Contactos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Nosotros().setVisible(true);
+                new Contactos().setVisible(true);
             }
         });
     }
